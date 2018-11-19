2c9ee2f9ce20f7a2df77ab13fe99a3060846f84f
amanjain2898@gmail.com <amanjain2898@gmail.com>
2018-11-20 02:17:37 +0530

    Pulled

--- code/confidence.m
+++ code/confidence.m
@@ -9,9 +9,9 @@ function [cp] = confidence(psi,x,y,confidence_mat)
     [rows,cols] = size(confidence_mat);
 
     min_x = max(1,x-psi);
-    max_x = min(cols,x+psi);
+    max_x = min(rows,x+psi);
     min_y = max(1,y-psi);
-    max_y = min(rows,y+psi);
+    max_y = min(cols,y+psi);
 
     cp =  sum(sum(confidence_mat(min_x:max_x,min_y:max_y)));
     cp = cp./(4*psi*psi);
