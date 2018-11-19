c72d3645fbc7520d5606b4657fc7c2d92a7db562
JagadeepSai <jagadeepsai28@gmail.com>
2018-11-19 15:12:29 +0530

    Changes in norm

--- code/norm_vec.m
+++ code/norm_vec.m
@@ -1,25 +1,26 @@
 function vec = norm_vec(counter_list,pixel)
 
 %constant 
+% counter_list = [ 1,1 ; 2,2 ; 3,3]';
+% pixel = [2,2];
 n = 3;
 sigma = 5;
 %----
 x = pixel(1); y = pixel(2);
 window = zeros([n,n]);
 dist = floor(n/2);
-for p = counter_list
+for p = counter_list'
     x2 = p(1) - x;
     y2 = p(2) - y;
     if( (abs(x2) <= dist) && (abs(y2) <= dist))
         window(dist+1+x2,dist+1+y2) = 1;
     end
 end
-window
-window = imgaussfilt(window,sigma);
 
+window = imgaussfilt(window,sigma);
+% window
 [fgrad_x,fgrad_y] = gradient(window);
 
-% [fgrad_x,fgrad_y]
 fx = fgrad_x(1);
 fy = fgrad_y(1);
 
@@ -28,6 +29,6 @@ if fx == 0 && fy == 0
 else
     vec = [fx; fy] / norm([fx; fy]);
 end
-
+vec = vec';
 end
 
